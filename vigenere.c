ac4db57cc72b5bc1afb82006ffc0355032934708
luay-software <su27lz@student.aau.dk>
2025-12-02 13:13:13 +0100

    1st commit

---
+++ vigenere.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+// encryption funktion
+
+
+// decryption funktion
+
+
+// Get-value funktion der tager en char som input og spytter en value ud som int
+
+
+// Get-char funktion der tager en int og spytter en char ud
+
+
+// Funktion der looper gennem tekst og kryptere den. Har tekst som input og keyword som input.
+
+
+// Evt. flere funktioner
