2dde33b93eb3192bfab1879d8759a30f2793ea7a
luay-software <su27lz@student.aau.dk>
2025-12-02 13:48:13 +0100

    2nd commit

--- vigenere.c
+++ vigenere.c
@@ -4,16 +4,19 @@
 #include <math.h>
 
 
-// encryption funktion
+// encryption funktion. tager en char og int som input og retuner en char.
 
 
-// decryption funktion
+// decryption funktion. tager en char og int som input og retuner en char.
 
 
-// Get-value funktion der tager en char som input og spytter en value ud som int
+// Get-value funktion der tager en char som input og spytter en value ud som int.
+// I ascii-kode har a har værdien 97 værdien og A har værdien 65, men de er begge indeks nul og skal retunere 0. b og B skal retunere 1 osv.
+// Lav evt. en switch case.
 
 
-// Get-char funktion der tager en int og spytter en char ud
+// Get-char funktion der tager en int og spytter en char ud.
+// Tager et tal ind fra 0-25 og spytter en char ud alt efter dets index. a / A = 0, og z / Z = 25.
 
 
 // Funktion der looper gennem tekst og kryptere den. Har tekst som input og keyword som input.
